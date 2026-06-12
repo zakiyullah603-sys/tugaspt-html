84d8a9b410f112ee72ac8b02e01d0e0d885b98ad
Ruhania <ruhania486@gmail.com>
2026-06-12 23:14:52 +0800

    nambah servernya kelompok16.js

--- kelompok16.js
+++ kelompok16.js
@@ -30,3 +30,32 @@ const menu = [
 ]
 
 module.exports = menu
+const express = require('express')
+const path = require('path')
+
+const menu = require('./data')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/api/menu', (req,res)=>{
+  res.json(menu)
+})
+
+const PORT = 3000
+
+app.listen(PORT, ()=>{
+  console.log(Server berjalan di http://localhost:${PORT})
+})
+{
+  "name": "rasa-kopi",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2"
+  }
+}
