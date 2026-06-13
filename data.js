ec691c055d1dbf0f348a7b00430e06df9c4ffa6c
zakiyullah603-sys <zakiyullah603@gmail.com>
2026-06-13 09:40:24 +0800

    03

---
+++ data.js
@@ -0,0 +1,171 @@
+/* DATA MENU RASA KOPI */
+
+const menuData = [
+
+  /* KOPI */
+
+  {
+    name: "Kopi Susu Gula Aren",
+    price: "Rp 18.000",
+    image: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=800"
+  },
+
+  {
+    name: "Americano Ice",
+    price: "Rp 17.000",
+    image: "https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?q=80&w=800"
+  },
+
+  {
+    name: "Caramel Latte",
+    price: "Rp 22.000",
+    image: "https://images.unsplash.com/photo-1494314671902-399b18174975?q=80&w=800"
+  },
+
+  {
+    name: "Vanilla Latte",
+    price: "Rp 24.000",
+    image: "https://images.unsplash.com/photo-1521302200778-33500795e128?q=80&w=800"
+  },
+
+  {
+    name: "Mocha Cream",
+    price: "Rp 25.000",
+    image: "https://images.unsplash.com/photo-1517701604599-bb29b565090c?q=80&w=800"
+  },
+
+  {
+    name: "Chocolate Coffee",
+    price: "Rp 26.000",
+    image: "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=800"
+  },
+
+  {
+    name: "Espresso Premium",
+    price: "Rp 15.000",
+    image: "https://images.unsplash.com/photo-1511920170033-f8396924c348?q=80&w=800"
+  },
+
+  {
+    name: "Hazelnut Latte",
+    price: "Rp 27.000",
+    image: "https://images.unsplash.com/photo-1498804103079-a6351b050096?q=80&w=800"
+  },
+
+  {
+    name: "Cappuccino",
+    price: "Rp 23.000",
+    image: "https://images.unsplash.com/photo-1534778101976-62847782c213?q=80&w=800"
+  },
+
+  {
+    name: "Kopi Pandan",
+    price: "Rp 20.000",
+    image: "https://images.unsplash.com/photo-1453614512568-c4024d13c247?q=80&w=800"
+  },
+
+  {
+    name: "Brown Sugar Coffee",
+    price: "Rp 24.000",
+    image: "https://images.unsplash.com/photo-1485808191679-5f86510681a2?q=80&w=800"
+  },
+
+  {
+    name: "Matcha Coffee Fusion",
+    price: "Rp 28.000",
+    image: "https://images.unsplash.com/photo-1517701550927-30cf4ba1f4d6?q=80&w=800"
+  },
+
+  /* SNACK */
+
+  {
+    name: "French Fries",
+    price: "Rp 16.000",
+    image: "https://images.unsplash.com/photo-1573080496219-bb080dd4f877?q=80&w=800"
+  },
+
+  {
+    name: "Croissant Butter",
+    price: "Rp 18.000",
+    image: "https://images.unsplash.com/photo-1509440159596-0249088772ff?q=80&w=800"
+  },
+
+  {
+    name: "Donat Coklat",
+    price: "Rp 14.000",
+    image: "https://images.unsplash.com/photo-1551024601-bec78aea704b?q=80&w=800"
+  },
+
+  {
+    name: "Cheese Cake",
+    price: "Rp 26.000",
+    image: "https://images.unsplash.com/photo-1533134242443-d4fd215305ad?q=80&w=800"
+  },
+
+  {
+    name: "Roti Bakar Coklat",
+    price: "Rp 17.000",
+    image: "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=800"
+  },
+
+  {
+    name: "Chicken Nugget",
+    price: "Rp 20.000",
+    image: "https://images.unsplash.com/photo-1562967916-eb82221dfb92?q=80&w=800"
+  },
+
+  {
+    name: "Dimsum Ayam",
+    price: "Rp 22.000",
+    image: "https://images.unsplash.com/photo-1563245372-f21724e3856d?q=80&w=800"
+  },
+
+  {
+    name: "Burger Beef",
+    price: "Rp 30.000",
+    image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?q=80&w=800"
+  },
+
+  {
+    name: "Toast Strawberry",
+    price: "Rp 18.000",
+    image: "https://images.unsplash.com/photo-1484723091739-30a097e8f929?q=80&w=800"
+  },
+
+  {
+    name: "Pisang Crispy",
+    price: "Rp 15.000",
+    image: "https://images.unsplash.com/photo-1604908176997-431f0d2d6d3c?q=80&w=800"
+  },
+
+  {
+    name: "Onion Ring",
+    price: "Rp 19.000",
+    image: "https://images.unsplash.com/photo-1639024471283-03518883512d?q=80&w=800"
+  },
+
+  {
+    name: "Chicken Wings",
+    price: "Rp 29.000",
+    image: "https://images.unsplash.com/photo-1608039755401-742074f0548d?q=80&w=800"
+  },
+
+  {
+    name: "Tiramisu Dessert",
+    price: "Rp 25.000",
+    image: "https://images.unsplash.com/photo-1571877227200-a0d98ea607e9?q=80&w=800"
+  },
+
+  {
+    name: "Mie Pedas Rasa Kopi",
+    price: "Rp 21.000",
+    image: "https://images.unsplash.com/photo-1612929633738-8fe44f7ec841?q=80&w=800"
+  },
+
+  {
+    name: "Sosis Bakar",
+    price: "Rp 16.000",
+    image: "https://images.unsplash.com/photo-1529193591184-b1d58069ecdd?q=80&w=800"
+  }
+
+];
